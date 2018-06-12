8164e1829b6f41787646b6e933265bbcdd680c18
tyhser <tyhser@hotmail.com>
2018-06-12 17:47:31 +0800

    1747

--- src/main.c
+++ src/main.c
@@ -5,18 +5,30 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
+#define mainSENDER_1 1
+#define mainSENDER_2 2
 
 xQueueHandle xQueue;
+typedef struct
+{
+	unsigned char ucValue;
+	unsigned char ucSource;
+} xData;
+
+static const xData xStructsToSend[2] = 
+{
+	{100, mainSENDER_1},
+	{200, mainSENDER_2}
+};
+
 static void vSenderTask(void *pvParameters)
 {
-	long lValueToSend;
 	portBASE_TYPE xStatus;
-
-	lValueToSend = (long)pvParameters;
+	const portTickType xTickToWait = 100 / portTICK_RATE_MS;
 	for (; ;)
 	{
 
-		xStatus = xQueueSendToBack(xQueue, &lValueToSend, 0);
+		xStatus = xQueueSendToBack(xQueue, pvParameters, xTickToWait);
 		if (xStatus != pdPASS)
 		{
 			printf("Could not send to the queue.\n");
@@ -27,43 +39,50 @@ static void vSenderTask(void *pvParameters)
 
 static void vReceiverTask(void *pvParameters)
 {
-	long lReceivedValue;
+	xData xReceivedStructure;
 	portBASE_TYPE xStatus;
-	const portTickType xTicksToWait = 100 / portTICK_RATE_MS;
 
-	for (; ;)
+	while (1)
 	{
-		if (uxQueueMessagesWaiting(xQueue) != 0)
-			printf("Queue should have been empty!\n");
-		xStatus = xQueueReceive(xQueue, &lReceivedValue, xTicksToWait);
+		if (uxQueueMessagesWaiting(xQueue) != 3)
+			printf("Queue should have been full!\n");
+
+		xStatus = xQueueReceive(xQueue, &xReceivedStructure, 0);
 		if (xStatus == pdPASS)
-			printf("Received = %ld\n", lReceivedValue);
-		else
 		{
-			printf("cound not receive from the queue.\n");
+			if(xReceivedStructure.ucSource == mainSENDER_1)
+			{
+				printf("From Sender 1 = %d\n", xReceivedStructure.ucValue);
+			}
+			else
+			{
+				printf("From Sender 2 = %d\n", xReceivedStructure.ucValue);
+			}
 		}
+		else
+			puts("Could not receive from the queue.\n");
 	}
 }
 
 
 int main(void)
 {
-	xQueue = xQueueCreate(5, sizeof(long));
+	xQueue = xQueueCreate(3, sizeof(xData));
 	if (xQueue != NULL)
 	{
 		xTaskCreate(vSenderTask, 
 				"Sender1",
 				1000,
-				(void *)100,
-				1,
+				&(xStructsToSend[0]), 	
+				2,
 				NULL);
 		xTaskCreate(vSenderTask, 
 				"Sender2",
 				1000,
-				(void *)200,
-				1,
+				&(xStructsToSend[1]), 	
+				2,
 				NULL);
-		xTaskCreate(vReceiverTask, "Receiver", 1000, NULL, 2, NULL);
+		xTaskCreate(vReceiverTask, "Receiver", 1000, NULL, 1, NULL);
 		vTaskStartScheduler();
 	}
 	else
@@ -72,8 +91,6 @@ int main(void)
 	}
 
 	for (; ;);
-
-	return 0;
 }
 
 void vMainQueueSendPassed(void)
